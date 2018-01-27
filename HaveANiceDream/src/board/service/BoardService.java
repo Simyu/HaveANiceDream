f01d59812712a2d6ba8c013b2aaa75d7d95b6046
김동혁 <HACKER@DESKTOP-6DSVGHI>
2018-01-28 02:48:30 +0900

    눈누 난나 게시판 리스트 끝!

--- HaveANiceDream/src/board/service/BoardService.java
+++ HaveANiceDream/src/board/service/BoardService.java
@@ -1,8 +1,12 @@
 package board.service;
 
+import java.util.ArrayList;
+
 import board.dto.BoardDTO;
+
 
 public interface BoardService {
 	int boardInsert(BoardDTO boardwrite);
-
+	
+	ArrayList<BoardDTO> boardList();
 }
