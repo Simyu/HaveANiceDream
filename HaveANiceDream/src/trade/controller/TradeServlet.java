5e5a02f4fc92a73bd95ea62f33352afce2d66ad0
jsy <jsy901001@gmail.com>
2018-01-30 18:46:42 +0900

    화이팅............

--- HaveANiceDream/src/trade/controller/TradeServlet.java
+++ HaveANiceDream/src/trade/controller/TradeServlet.java
@@ -26,6 +26,7 @@ public class TradeServlet extends HttpServlet {
 		System.out.println("연결 성공");
 		request.setCharacterEncoding("utf-8");
 		response.setContentType("text/html; charset=utf-8");
+		
 		//해당 서블릿에서 구현할 사항
 		//거래요청이 들어오면 구매자,판매자에게 문자전송
 		//물품리스트에서 (list서블릿에서 숨김기능)
@@ -34,13 +35,23 @@ public class TradeServlet extends HttpServlet {
 	 public static int  TRADE_END = 2;//거래종료   인 경우 마이페이지에서 확인이 가능해야함...  따라서 거래가 신청이 들어오면 현재상태를 변경하여 마이페이지에서 거래 상태 화면이 떠야함..
 	 거래상태를 변경시키는 업데이트 메소드
 		 * */
-		
-		
-		
-		
+		HttpSession ses = request.getSession(false);
 		String viewpath="";
+		if(ses!=null){
+			MemberDTO user = (MemberDTO) ses.getAttribute("user");
+			String userId= user.getUserId();
+			//유저가 등록한 물품리스트 출력
+			viewpath="../Trade/trade_list.jsp";
+		}else{
+			viewpath="../user/login.html";
+		}
 		
-		viewpath="../main/temp_main_con.jsp";
+		
+		
+		
+		
+		
+		
 		
 		request.setAttribute("viewpath", viewpath);
 		
