e37e00f267c8fab8bd1a68c1d3fe7f8bb158c2f7
jsy <jsy901001@gmail.com>
2018-02-01 18:13:08 +0900

    8

--- HaveANiceDream/src/product/service/ProductService.java
+++ HaveANiceDream/src/product/service/ProductService.java
@@ -7,9 +7,9 @@ import java.util.ArrayList;
 import product.dto.ProductDTO;
 
 public interface ProductService {
-	int insertProduct(ProductDTO product, String imageSrc);
+	int insertProduct(ProductDTO product, ArrayList<String> imageSrc);
 	ArrayList<ProductDTO>  product_List(String title ,int categoryNo,int  categoryDetailNo,String UserId);
 	ProductDTO productSelect(int productNo);
-	String productSelect_Image(int productNo);
+	ArrayList<String> productSelect_Image(int productNo);
 	int updateProduct(ProductDTO product, String imageSrc);
 }
