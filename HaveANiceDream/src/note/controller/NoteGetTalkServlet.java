5e13188551b4434af97921c0a686c5630dc9904f
SuJin Lee <leesuj28@gmail.com>
2018-02-05 05:21:22 +0900

    메신저 테스트필요..프로필 눌렀을시 메시지 적을 팝업 필요

---
+++ HaveANiceDream/src/note/controller/NoteGetTalkServlet.java
@@ -0,0 +1,52 @@
+package note.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import note.service.NoteService;
+import note.service.NoteServiceImpl;
+import user.dto.MemberDTO;
+
+@WebServlet(name = "note/getyoulist", urlPatterns = { "/note/getyoulist.do" })
+public class NoteGetTalkServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String userId = request.getParameter("userId");
+		
+		NoteService service = new NoteServiceImpl();
+		ArrayList<MemberDTO> list = service.noteToList(userId);
+		
+		JSONObject jsonObject = new JSONObject();
+		JSONArray array = new JSONArray();
+		
+		int size = list.size();
+		for (int i = 0; i < size; i++) {
+			MemberDTO dto = list.get(i);
+			JSONObject object = new JSONObject();
+
+			object.put("id",dto.getUserId());
+			object.put("name", dto.getUserName());
+			object.put("img", dto.getUserImage());
+			
+			array.add(object);
+		}
+		
+		jsonObject.put("list", array);
+		
+		PrintWriter printWriter = response.getWriter();
+		printWriter.println(jsonObject.toJSONString());
+	}
+
+}
